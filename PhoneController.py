0b2dcc0f4bf09110759719b1218c62293771cb54
suzhentxt <chaunguyentotrinh2006@gmail.com>
2024-08-15 15:32:24 +0700

    update'

--- PhoneController.py
+++ PhoneController.py
@@ -1,8 +1,5 @@
 import subprocess
 
-a = str(subprocess.check_output("adb devices", shell=True))
-a = a.replace("b'List of devices attached\\r\\n", "")
-a = a.replace("\\tdevice\\r", "")
-a = a.replace("\\n", "")
+a = str(subprocess.check_output("adb devices", shell=True)).replace("b'List of devices attached\\r\\n", "").replace("\\tdevice\\r", "").split("\\n")
 
 print(a[:-2])
