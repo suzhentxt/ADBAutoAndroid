3f111d14565a3a3b50dbc4d9090b2daeed942ee7
suzhentxt <chaunguyentotrinh2006@gmail.com>
2024-08-15 15:29:11 +0700

    update'

--- PhoneController.py
+++ PhoneController.py
@@ -1,5 +1,8 @@
 import subprocess
 
 a = str(subprocess.check_output("adb devices", shell=True))
+a = a.replace("b'List of devices attached\\r\\n", "")
+a = a.replace("\\tdevice\\r", "")
+a = a.replace("\\n", "")
 
-print(a.replace("b'List of devices attached\\r\\n", ""))
+print(a[:-2])
