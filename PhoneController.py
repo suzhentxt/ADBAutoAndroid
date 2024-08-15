a47fef8d51e2faa9c4cd6f76c6d9885f61e9de41
suzhentxt <chaunguyentotrinh2006@gmail.com>
2024-08-15 15:26:02 +0700

    update'

--- PhoneController.py
+++ PhoneController.py
@@ -1,3 +1,5 @@
 import subprocess
 
-print(subprocess.check_output("adb devices", shell=True))
+a = str(subprocess.check_output("adb devices", shell=True))
+
+print(a.replace("b'List of devices attached\\r\\n", ""))
